5d2b78b16dfe9cfccb1ab11702735fd96beeb594
Timofey Shpilka <wh1ter5713@gmail.com>
2024-11-01 20:20:36 +0200

    01.11.2024

--- src/main/java/com/example/bootshop/config/SecurityConfig.java
+++ src/main/java/com/example/bootshop/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/", "/register", "/login", "/profile", "/product/**", "/svg/**", "/css/**").permitAll()
+                .requestMatchers("/", "/register", "/login", "/profile", "/product/**", "/svg/**", "/css/**", "/img/**").permitAll()
                 .anyRequest().authenticated()
             )
             .formLogin((form) -> form
